ca27464aeef85023a675a5574b9a2934f7cb4bf1
Mikey Austin <mikey@mikeyaustin.com>
2024-04-19 20:38:40 +0200

    added first semi-working version of a podcast2webdav program

---
+++ cmd/podcast2webdav/main.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+	"strconv"
+
+	"github.com/mmcdole/gofeed"
+	"github.com/spf13/pflag"
+	"golang.org/x/net/webdav"
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	AppName string `yaml:"appName"`
+	Feeds   []struct {
+		Name string `yaml:"name"`
+		URL  string `yaml:"url"`
+	} `yaml:"feeds"`
+}
+
+func parseConfig(path string) (*Config, error) {
+	file, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	var config Config
+	err = yaml.Unmarshal(file, &config)
+	if err != nil {
+		return nil, err
+	}
+	return &config, nil
+}
+
+func fetchFeeds(config *Config) ([]*gofeed.Feed, error) {
+	parser := gofeed.NewParser()
+	var feeds []*gofeed.Feed
+	for _, feedConfig := range config.Feeds {
+		feed, err := parser.ParseURL(feedConfig.URL)
+		if err != nil {
+			log.Printf("Failed to fetch feed: %s", feedConfig.Name)
+			continue
+		}
+		feeds = append(feeds, feed)
+	}
+	log.WithField("Feeds", len(feeds)).Info("re-loaded podcast feeds")
+	return feeds, nil
+}
+
+func main() {
+	configFile := pflag.String("config", "podcast2rygel.yaml", "the main config file containing the feeds")
+	verbose := pflag.Bool("verbose", false, "enables more debug information")
+	pflag.Parse()
+
+	if *verbose {
+		log.SetLevel(log.DebugLevel)
+	} else {
+		log.SetLevel(log.InfoLevel)
+	}
+
+	config, err := parseConfig(*configFile)
+	if err != nil {
+		log.Fatalf("Failed to parse config: %v", err)
+	}
+	log.WithField("config", config).WithField("file", configFile).Debug("parsed configuration file")
+
+	// Create an in-memory filesystem
+	fs := webdav.NewMemFS()
+
+	// Populate the memfs with audio files (fetch on demand)
+	feeds, err := fetchFeeds(config)
+	if err != nil {
+		log.Fatalf("Failed to fetch RSS feeds: %v", err)
+	}
+
+	context := context.Background()
+	episodeUrlMap := make(map[string]*gofeed.Item)
+	fs.Mkdir(context, "/podcasts", os.ModePerm)
+	for i, feedConfig := range config.Feeds {
+		feedBasePath := "/podcasts/" + feedConfig.Name
+		fs.Mkdir(context, feedBasePath, os.ModePerm)
+		for j, item := range feeds[i].Items {
+			// TODO: make an extension according to the mime type
+			episodePath := feedBasePath + "/episode" + strconv.Itoa(j) + ".mp3"
+			episodeFile, err := fs.OpenFile(context, episodePath, os.O_CREATE|os.O_RDWR, 0644)
+			if err != nil {
+				log.Fatalf("Failed to create episode path: "+episodePath, err)
+			}
+			episodeFile.Write([]byte{})
+			episodeUrlMap[episodePath] = item
+		}
+	}
+
+	// Create a WebDAV handler
+	handler := &webdav.Handler{
+		FileSystem: fs,
+		LockSystem: webdav.NewMemLS(),
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Handle WebDAV requests
+		switch r.Method {
+		case http.MethodGet:
+			// Serve audio files dynamically
+			webdavFilePath := r.URL.Path
+			if strings.HasPrefix(webdavFilePath, "/podcasts/") {
+				episode := episodeUrlMap[webdavFilePath]
+				if episode == nil {
+					return
+				}
+				resp, err := http.Get(episode.Enclosures[0].URL)
+				if err != nil {
+					http.Error(w, "Error fetching audio content", http.StatusInternalServerError)
+					return
+				}
+				defer resp.Body.Close()
+
+				// Set appropriate headers for audio file
+				w.Header().Set("Content-Type", episode.Enclosures[0].Type)
+				w.Header().Set("Content-Length", fmt.Sprintf("%d", resp.ContentLength))
+
+				// Stream audio content directly to the response
+				_, _ = io.Copy(w, resp.Body)
+				return
+			}
+		}
+
+		// Serve WebDAV requests
+		handler.ServeHTTP(w, r)
+	})
+
+	port := 8080
+	fmt.Printf("WebDAV server listening on port %d...\n", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+}
---
+++ go.mod
@@ -0,0 +1,21 @@
+module podcast2webdav
+
+go 1.21.9
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/liamg/memoryfs v1.6.0 // indirect
+	github.com/mmcdole/gofeed v1.3.0 // indirect
+	github.com/mmcdole/goxpp v1.1.1-0.20240225020742-a0c311522b23 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/spf13/afero v1.11.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
